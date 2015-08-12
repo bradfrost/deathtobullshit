70d46abf612efc2f986986ee3dff4df6d80bf694
Ian Frost <inf5000@gmail.com>
2015-08-12 18:13:48 -0400

    Set up initial conditions to "Turn on bullshit?" button
    Set up general idea for "Turn on bullshit?" button

---
+++ js/bullshit.js
@@ -0,0 +1,25 @@
+
+var bullshitButton = document.getElementById("bullshitbutton");
+var bullshitMode = false;
+bullshitButton.addEventListener("click", function(event){
+	if(bullshitMode === false){
+		if (confirm("Are you sure you want to see ALL of the bullshit?!")) {
+			bullshitMode = true;
+			event.target.innerHTML = "Please turn this bullshit off!";
+			runParallax();
+		}
+		else {
+			console.log("Thank God");
+		}
+	} 
+	else {
+		bullshitMode = false;
+		console.log("Good work.");
+		event.target.innerHTML = "Turn bullshit on?";
+	}
+})
+
+function runParallax() {
+	console.log("I'm running parallax. I suck.");
+}
+
