febd32e1a8e5386c72c127f22241589bfea7e5bc
Ian Frost <inf5000@gmail.com>
2015-08-17 11:35:29 -0400

    Added social buttons to posts
    Used functions and for loops in JS to add and remove social media images when bullshit button is turned on and off.

--- js/bullshit.js
+++ js/bullshit.js
@@ -3,19 +3,24 @@ var bullshitButton = document.getElementById("bullshitbutton");
 var bullshitMode = false;
 bullshitButton.addEventListener("click", function(event){
 	if(bullshitMode === false){
+		// Turn bullshit on
 		if (confirm("Are you sure you want to see ALL of the bullshit?!")) {
 			bullshitMode = true;
 			event.target.innerHTML = "Please turn this bullshit off!";
 			runParallax();
+			runSocial();
 		}
 		else {
+			// User selected cancel
 			console.log("Thank God");
 		}
 	} 
 	else {
+		// Turn bullshit off
 		bullshitMode = false;
 		console.log("Good work.");
 		event.target.innerHTML = "Turn bullshit on?";
+		killSocial();
 	}
 })
 
@@ -23,9 +28,27 @@ function runParallax() {
 	console.log("I'm running parallax. I suck.");
 }
 
-window.onscroll = function() {
-	console.log(window.pageYOffset);
+/* window.onscroll = function() {
+	 console.log(window.pageYOffset); 
+} */
+
+function runSocial() {
+	var posts = document.querySelectorAll('.post');
+	for (i = 0; i < posts.length; ++i) {
+		var social = document.createElement('div');
+		social.className = 'social';
+		posts[i].appendChild(social);
+	}
+	
 }
+
+function killSocial() {
+	var social = document.querySelectorAll('.social')
+	for (i = 0; i < social.length; ++i){
+		social[i].remove(social);
+	}
+}
+
 //Parallax script
 var s = skrollr.init();
 
